3a66fb4cecaf9d183218982d6904250b79ddc0f0
Bret Young <hby@acm.org>
2016-01-03 21:37:52 -0600

    Some doc strings for private funcitons. Added codox plugin.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :profiles {:test
-             {:dependencies [[org.clojure/test.check "0.7.0"]]}})
+  :profiles {:test {:dependencies [[org.clojure/test.check "0.7.0"]]}
+             :dev {:plugins [[lein-codox "0.9.1"]]}})
--- src/com/inferstructure/repl.clj
+++ src/com/inferstructure/repl.clj
@@ -19,14 +19,23 @@
     (seq? mvsors) (explore-seq threshold peekn mvsors)
     :else mvsors))
 
+;;; In hindsight, I realize I could use multimethods.
+;;; I may make that update in the future.
+
 (declare big-map)
 (defn- explore-map
+  "Used by com.inferstructure.repl/explore to safely explore
+   potentially large maps."
   [t p m]
   (if (< (count m) t)
     (into {} (map (fn [[key val]] [key (explore t p val)]) m))
     (big-map t p m)))
 
 (defn- big-map
+  "Return a map with p+1 entries.
+   p entries, [k v], come from m and have the values replaced with a call
+   to (com.inferstructure.repl/explore t p v).
+   Another entry is [:more \"n more entries \"] where n is (- (count m) p)."
   [t p m]
   (let [peeked (take p m)
         rest (drop p m)
@@ -37,12 +46,17 @@
 
 (declare big-vector)
 (defn- explore-vector
+  "Used by com.inferstructure.repl/explore to safely explore
+   potentially large vectors."
   [t p v]
   (if (< (count v) t)
     (into [] (map (fn [e] (explore t p e)) v))
     (big-vector t p v)))
 
 (defn- big-vector
+  "Return a vector of p+1 elements.
+   p elements come from calling (com.inferstructure.repl/explore t p e) on the first p elements (e) of v.
+   Another element (the last) is \"n more entries \" where n is (- (count v) p)"
   [t p v]
   (let [peeked (take p v)
         rest (drop p v)
@@ -52,12 +66,18 @@
 
 (declare big-set)
 (defn- explore-set
+  "Used by com.inferstructure.repl/explore to safely explore
+   potentially large sets."
   [t p s]
   (if (< (count s) t)
     (into #{} (map (fn [e] (explore t p e)) s))
     (big-set t p s)))
 
 (defn- big-set
+  "Return a set with p+1 elements.
+   p elements (e) come from s and are the result of a call
+   to (com.inferstructure.repl/explore t p e).
+   Another element is \"n more entries \" where n is (- (count s) p)."
   [t p s]
   (let [peeked (take p s)
         rest (drop p s)
@@ -67,6 +87,8 @@
 
 (declare big-seq)
 (defn- explore-seq
+  "Used by com.inferstructure.repl/explore to safely explore
+   potentially large sequences."
   [t p s]
   (let [front (take t s)
         more (drop t s)]
@@ -76,6 +98,9 @@
       (big-seq t p s))))
 
 (defn- big-seq
+  "Return a sequence of p+1 elements.
+   p elements come from calling (com.inferstructure.repl/explore t p e) on the first p elements (e) of v.
+   Another element (the last) is \"more ...\""
   [t p s]
   (let [peeked (take p s)
         more "more ..."]
