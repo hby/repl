2b7a4e5493bd02e236c76d5e3af219beab0e398d
Bret Young <hby@acm.org>
2016-01-04 18:04:17 -0600

    Reimplement with multimethods.

--- src/com/inferstructure/repl.clj
+++ src/com/inferstructure/repl.clj
@@ -1,41 +1,66 @@
 (ns com.inferstructure.repl)
 
-(declare explore-map)
-(declare explore-vector)
-(declare explore-set)
-(declare explore-seq)
-(defn explore
-  "Safely explore possibly large map/vector/set/seq structures in the repl.
-   threshold - if count is under threshold, recursively explore
-   peekn - if over threshold, recursivley explore peekn entries
-           and summarize the rest
-   mvsors - map, vector, set, or seq"
-  [threshold peekn mvsors]
-  {:pre [(> threshold peekn)]}
-  (cond
-    (map? mvsors) (explore-map threshold peekn mvsors)
-    (vector? mvsors) (explore-vector threshold peekn mvsors)
-    (set? mvsors) (explore-set threshold peekn mvsors)
-    (seq? mvsors) (explore-seq threshold peekn mvsors)
-    :else mvsors))
+(derive clojure.lang.PersistentArrayMap ::Map)
+(derive clojure.lang.PersistentVector ::Vector)
+(derive clojure.lang.PersistentHashSet ::Set)
+(derive clojure.lang.Iterate ::Seq)
+(derive clojure.lang.IPersistentList ::Seq)
 
-;;; In hindsight, I realize I could use multimethods.
-;;; I may make that update in the future.
+(defmulti explore
+          "Safely explore possibly large map/vector/set/seq structures in the repl.
+           threshold - if count is under threshold, recursively explore
+           peekn - if over threshold, recursivley explore peekn entries
+                   and summarize the rest
+           mvsors - map, vector, set, or seq"
+          {:arglists '([threshold peekn mvsors])}
+          (fn [_ _ c] (class c)))
 
-(declare big-map)
-(defn- explore-map
-  "Used by com.inferstructure.repl/explore to safely explore
-   potentially large maps."
+(defmulti big
+          "Used by explore to summaraize a structure with more than threshold
+          elements by building a new structure with peekn + 1 elements.
+          Generally, peekn elements (e) come from mvsors and are the result of
+          calling (explore t p e).
+          The other element is either \"n more entries\", where n is (- (count mvsors) p),
+          or \"more ...\" if (count mvsors) cannot be known.
+
+          threshold - mvsors is assumed to be > than threshold, passed to calls of explore
+          peekn - recursivley explore peekn entries and summarize the rest
+          mvsors - map, vector, set, or seq"
+          {:arglists '([threshold peekn mvsors])}
+          (fn [_ _ c] (class c)))
+
+(defmethod explore ::Map
   [t p m]
   (if (< (count m) t)
     (into {} (map (fn [[key val]] [key (explore t p val)]) m))
-    (big-map t p m)))
+    (big t p m)))
 
-(defn- big-map
-  "Return a map with p+1 entries.
-   p entries, [k v], come from m and have the values replaced with a call
-   to (com.inferstructure.repl/explore t p v).
-   Another entry is [:more \"n more entries \"] where n is (- (count m) p)."
+(defmethod explore ::Vector
+  [t p v]
+  (if (< (count v) t)
+    (into [] (map (fn [e] (explore t p e)) v))
+    (big t p v)))
+
+(defmethod explore ::Set
+  [t p s]
+  (if (< (count s) t)
+    (into #{} (map (fn [e] (explore t p e)) s))
+    (big t p s)))
+
+(defmethod explore ::Seq
+  [t p s]
+  (let [front (take t s)
+        more (drop t s)]
+    (if (and (< (count front) t)
+             (empty? more))
+      (map (fn [e] (explore t p e)) s)
+      (big t p s))))
+
+(defmethod explore :default
+  [_ _ v]
+  v)
+
+(defmethod big ::Map
   [t p m]
   (let [peeked (take p m)
         rest (drop p m)
@@ -44,19 +69,7 @@
                  (map (fn [[key val]] [key (explore t p val)]) peeked))
       :more more)))
 
-(declare big-vector)
-(defn- explore-vector
-  "Used by com.inferstructure.repl/explore to safely explore
-   potentially large vectors."
-  [t p v]
-  (if (< (count v) t)
-    (into [] (map (fn [e] (explore t p e)) v))
-    (big-vector t p v)))
-
-(defn- big-vector
-  "Return a vector of p+1 elements.
-   p elements come from calling (com.inferstructure.repl/explore t p e) on the first p elements (e) of v.
-   Another element (the last) is \"n more entries \" where n is (- (count v) p)"
+(defmethod big ::Vector
   [t p v]
   (let [peeked (take p v)
         rest (drop p v)
@@ -64,20 +77,7 @@
     (conj (mapv (fn [e] (explore t p e)) peeked)
           more)))
 
-(declare big-set)
-(defn- explore-set
-  "Used by com.inferstructure.repl/explore to safely explore
-   potentially large sets."
-  [t p s]
-  (if (< (count s) t)
-    (into #{} (map (fn [e] (explore t p e)) s))
-    (big-set t p s)))
-
-(defn- big-set
-  "Return a set with p+1 elements.
-   p elements (e) come from s and are the result of a call
-   to (com.inferstructure.repl/explore t p e).
-   Another element is \"n more entries \" where n is (- (count s) p)."
+(defmethod big ::Set
   [t p s]
   (let [peeked (take p s)
         rest (drop p s)
@@ -85,22 +85,7 @@
     (conj (into #{} (map (fn [e] (explore t p e)) peeked))
           more)))
 
-(declare big-seq)
-(defn- explore-seq
-  "Used by com.inferstructure.repl/explore to safely explore
-   potentially large sequences."
-  [t p s]
-  (let [front (take t s)
-        more (drop t s)]
-    (if (and (< (count front) t)
-             (empty? more))
-      (map (fn [e] (explore t p e)) s)
-      (big-seq t p s))))
-
-(defn- big-seq
-  "Return a sequence of p+1 elements.
-   p elements come from calling (com.inferstructure.repl/explore t p e) on the first p elements (e) of v.
-   Another element (the last) is \"more ...\""
+(defmethod big ::Seq
   [t p s]
   (let [peeked (take p s)
         more "more ..."]
