53835c5a145f490460341d868a5f76de914952fe
Bret Young <hby@acm.org>
2018-10-04 23:37:21 -0500

    project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.inferstructure/repl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Some REPL help"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
